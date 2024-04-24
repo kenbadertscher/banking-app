06fa971444de66908706e95389f23cae82d5bf97
kenbadertscher <badertscher.ken@gmail.com>
2024-04-23 21:25:22 -0400

    implement HeaderBox welcome and subtext splash

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import HeaderBox from '../../components/HeaderBox'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Ken' };
+
   return (
     <section className="home">
       <div className="home-content"> 
         <header className="home-header">
-          Weclome, Ken
+          <HeaderBox 
+            type="greeting"
+            title="Welcome,"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions effeciently."
+          />
         </header>
       </div>
     </section>
