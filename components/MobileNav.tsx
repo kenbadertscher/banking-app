44da0b32c60030f38b065c7a147754512d59f3fb
kenbadertscher <badertscher.ken@gmail.com>
2024-05-02 22:08:31 -0400

    implement mobile nav bar changes

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import {
+  Sheet,
+  SheetTrigger,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetDescription,
+} from "./ui/sheet"
 
 const MobileNav = ({ user }: MobileNavProps) => {
   return (
-    <div>MobileNav</div>
+    <section>
+      <Sheet>
+        <SheetTrigger>Open</SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely certain</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone.  Remove your data.
+          </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </section>
   )
 }
 
