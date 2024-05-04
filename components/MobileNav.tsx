4814570f0c28dc5cef9a5ba4daac52ee71b0831e
kenbadertscher <badertscher.ken@gmail.com>
2024-05-03 22:16:25 -0400

    implement more mobile nav changes

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import Image from "next/image"
 import {
   Sheet,
   SheetTrigger,
@@ -6,19 +9,59 @@ import {
   SheetTitle,
   SheetDescription,
 } from "./ui/sheet"
+import { Link } from "lucide-react"
+import { sidebarLinks } from "../constants"
+import { usePathname } from "next/navigation"
+import { cn } from "lib/utils";
 
 const MobileNav = ({ user }: MobileNavProps) => {
+  const pathname = usePathname();
+
   return (
-    <section>
+    <section className="w-full max-w-[264px]">
       <Sheet>
-        <SheetTrigger>Open</SheetTrigger>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Are you absolutely certain</SheetTitle>
-          <SheetDescription>
-            This action cannot be undone.  Remove your data.
-          </SheetDescription>
-          </SheetHeader>
+        <SheetTrigger>
+          <Image 
+            src="/icons/hamburger.svg"
+            width={30}
+            height={30}
+            alt="menu"
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent side="left">
+        <Link href="/" className="mb-12 flex cursor-pointer items-center gap-2">
+          <Image 
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="Horizon logo"
+            className="size-[24px] max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">Horizon</h1>
+        </Link>
+
+        {sidebarLinks.map((item) => {
+          const isActive = pathname ===item.route || pathname.startsWith(`${item.route}/`)
+
+          return (
+            <Link href={item.route} key={item.label}
+              className={cn('sidebar-link', {'bg-bank-gradient':isActive})}
+            >
+              <div className="relative size-6">
+                <Image 
+                  src={item.imgURL}
+                  alt={item.label}
+                  fill
+                  className={cn({'brightness-[3] invert-0': isActive})}
+                />
+              </div>
+              <p className={cn('sidebar-label', {'!text-white' :isActive})}>
+                {item.label}
+              </p>
+            </Link>
+          )
+        })}
         </SheetContent>
       </Sheet>
     </section>
