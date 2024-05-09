0669aad32902630104cd7988e5f1dba791370bfc
kenbadertscher <badertscher.ken@gmail.com>
2024-05-08 21:20:56 -0400

    adjust bank properties for temp deployment

--- types/index.d.ts
+++ types/index.d.ts
@@ -86,15 +86,19 @@ declare type Transaction = {
   receiverBankId: string;
 };
 
+// declare type Bank = {
+//   $id: string;
+//   accountId: string;
+//   bankId: string;
+//   accessToken: string;
+//   fundingSourceUrl: string;
+//   userId: string;
+//   sharableId: string;
+// };
+
 declare type Bank = {
-  $id: string;
-  accountId: string;
-  bankId: string;
-  accessToken: string;
-  fundingSourceUrl: string;
-  userId: string;
-  sharableId: string;
-};
+  currentBalance: number;
+}
 
 declare type AccountTypes =
   | "depository"
