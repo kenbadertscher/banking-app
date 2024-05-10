44f6c191413256326f1bf18ded9b6ad094f501d7
kenbadertscher <badertscher.ken@gmail.com>
2024-05-09 21:43:52 -0400

    implement sign in form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -4,9 +4,40 @@ import Image from "next/image"
 import Link from "next/link"
 import React, { useState } from 'react'
 
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Control, useForm } from "react-hook-form";
+
+import { Button } from "./ui/button";
+import {
+  Form
+} from "components/ui/form"
+import CustomInput from "./CustomInput";
+import { authFormSchema } from "lib/utils";
+
+const formSchema = z.object({
+  email: z.string().email()
+})
+
 const AuthForm = ({ type }: { type: string}) => {
   const [user, setUser] = useState(null)
 
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof authFormSchema>>({
+    resolver: zodResolver(authFormSchema),
+    defaultValues: {
+      email: "",
+      password: ""
+    },
+  })
+  
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof authFormSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+  
   return (
     <section className="auth-form">
       <header className="flex flex-col gap-5 md:gap-8">
@@ -44,7 +75,25 @@ const AuthForm = ({ type }: { type: string}) => {
         </div>
       ): (
         <>
-          FORM
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              {/* Email Form Field */}
+              <CustomInput 
+                control={form.control}
+                name='email'
+                label="Email"
+                placeholder='Enter your email'
+              />
+              {/* Password Form Field */}
+              <CustomInput 
+                control={form.control}
+                name='password'
+                label="Password"
+                placeholder='Enter your password'
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
         </>
       )}
     </section>
